9b9c347970a3de65c27d0d7ee856e608889532d7
stefuthomas <142782594+stefuthomas@users.noreply.github.com>
2024-01-22 20:11:22 +0200

    Enum-määritykset tehty tapahtumille.

--- Tehtava sarja2/src/TapahtumaOhjelma.java
+++ Tehtava sarja2/src/TapahtumaOhjelma.java
@@ -1,12 +1,18 @@
 import java.util.PriorityQueue;
 
+enum TapahtumanTyyppi {
+    saapuminen, poistuminen
+}
+
 class Tapahtuma implements Comparable<Tapahtuma> {
     // Tapahtuma-luokan attribuutit
     private String nimi;
-    private long aika;
-    public Tapahtuma(String nimi, long aika) { // Konstruktori
+    private int aika;
+    public TapahtumanTyyppi tyyppi;
+    public Tapahtuma(String nimi, int aika) { // Konstruktori
         this.nimi = nimi;
         this.aika = aika;
+        this.tyyppi = TapahtumanTyyppi.saapuminen;
     }
 
     public String getNimi() {   // Getteri
@@ -39,21 +45,25 @@ class TapahtumaLista {
 
     public void addTapahtuma(Tapahtuma tapahtuma) { // Metodi, jolla lisätään tapahtuma tapahtumalistaan.
         tapahtumat.add(tapahtuma);
-        System.out.println(tapahtuma.getNimi() + " lisätty. " + tapahtuma.getAika() + " minuuuttia");
+        System.out.print(tapahtuma.getNimi() + " lisätty. " + tapahtuma.getAika() + " minuuuttia");
+        System.out.println(" Tyyppi: " + tapahtuma.tyyppi);
     }
 
-    public Tapahtuma nextTapahtuma() {
-        return tapahtumat.peek();
-    }   // Metodi, jolla palautetaan seuraavaksi tapahtuva tapahtuma ilman poistoa.
+    public Tapahtuma nextTapahtuma() {  // Metodi, jolla palautetaan seuraavaksi tapahtuva tapahtuma ilman poistoa.
+        Tapahtuma poistuva = tapahtumat.peek();
+        poistuva.tyyppi = TapahtumanTyyppi.poistuminen;
+        return poistuva;
+    }
 
-    public void removeNextTapahtuma() {
+    public void removeNextTapahtuma() { // Metodi, jolla poistetaan seuraavaksi tapahtuva tapahtuma.
+        System.out.println(" Tyyppi: " + tapahtumat.peek().tyyppi);
         tapahtumat.poll();
-    }   // Metodi, jolla poistetaan seuraavaksi tapahtuva tapahtuma.
+    }
 
     public void printTapahtumat() { // Metodi, jolla tulostetaan kaikki tapahtumat, jos lista ei ole tyhjä.
-        while (!tapahtumat.isEmpty()) {
-            Tapahtuma tapahtuma = tapahtumat.poll();
-            System.out.println(tapahtuma.getNimi() + " - Aika: " + tapahtuma.getAika() + "minuuttia");
+            while (!tapahtumat.isEmpty()) {
+                Tapahtuma tapahtuma = tapahtumat.poll();
+                System.out.println(tapahtuma.getNimi() + " - Aika: " + tapahtuma.getAika() + " minuuttia" + " Tyyppi: " + tapahtuma.tyyppi);
         }
     }
 
@@ -63,21 +73,22 @@ public class TapahtumaOhjelma { // Pääohjelma-luokka, jossa testataan Tapahtum
     public static void main(String[] args) {
         TapahtumaLista tapahtumaLista = new TapahtumaLista();   // Luodaan uusi tapahtumalista-olio.
         // Generoidaan tapahtumia ja lisätään ne tapahtumalistaan.
-        tapahtumaLista.addTapahtuma(new Tapahtuma("Tapahtuma 1", 15));
-        tapahtumaLista.addTapahtuma(new Tapahtuma("Tapahtuma 2", 9));
-        tapahtumaLista.addTapahtuma(new Tapahtuma("Tapahtuma 3", 11));
-        tapahtumaLista.addTapahtuma(new Tapahtuma("Tapahtuma 4", 20));
-        tapahtumaLista.addTapahtuma(new Tapahtuma("Tapahtuma 5", 4));
-        tapahtumaLista.addTapahtuma(new Tapahtuma("Tapahtuma 6", 7));
+        tapahtumaLista.addTapahtuma(new Tapahtuma("Tapahtuma 1", (int) (Math.random() * 25) + 1));
+        tapahtumaLista.addTapahtuma(new Tapahtuma("Tapahtuma 2", (int) (Math.random() * 25) + 1));
+        tapahtumaLista.addTapahtuma(new Tapahtuma("Tapahtuma 3", (int) (Math.random() * 25) + 1));
+        tapahtumaLista.addTapahtuma(new Tapahtuma("Tapahtuma 4", (int) (Math.random() * 25) + 1));
+        tapahtumaLista.addTapahtuma(new Tapahtuma("Tapahtuma 5", (int) (Math.random() * 25) + 1));
+        tapahtumaLista.addTapahtuma(new Tapahtuma("Tapahtuma 6", (int) (Math.random() * 25) + 1));
         // Seuraavaksi tapahtuva tapahtuma ilman poistoa.
-        System.out.println("Seuraava tapahtuma: " + tapahtumaLista.nextTapahtuma().getNimi());
+        System.out.print("Seuraava tapahtuma: " + tapahtumaLista.nextTapahtuma().getNimi());
         // Poistetaan seuraavaksi tapahtuva tapahtuma.
         tapahtumaLista.removeNextTapahtuma();
-        System.out.println("Seuraava tapahtuma: " + tapahtumaLista.nextTapahtuma().getNimi());
+        System.out.print("Seuraava tapahtuma: " + tapahtumaLista.nextTapahtuma().getNimi());
+        tapahtumaLista.removeNextTapahtuma();
+        System.out.print("Seuraava tapahtuma: " + tapahtumaLista.nextTapahtuma().getNimi());
         tapahtumaLista.removeNextTapahtuma();
         // Tulostetaan tapahtumat-listalla olevat tapahtumat.
         System.out.println("Tapahtumat:");
         tapahtumaLista.printTapahtumat();
-
     }
 }
